31fea80b641e1084f1aff3f40e1f9686311118cb
Saiganeshk15 <saiganeshk15@gmail.com>
2023-01-17 18:39:52 +0530

    nav set with judges page

--- nav.js
+++ nav.js
@@ -1,16 +1,3 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-navtools = document.querySelector('.navtools')
-rightnav = document.querySelector('.rightnav ')
-
-
-burger.addEventListener('click', () => {
-    navbar.classList.toggle('h-nav-resp')
-    navtools.classList.toggle('v-class-resp')
-    rightnav.classList.toggle('v-class-resp')
-
-})
-/**********faqs starts here***********/
 var faq = document.getElementsByClassName("faq-page");
 var i;
 for (i = 0; i < faq.length; i++) {
@@ -34,14 +21,17 @@ for (i = 0; i < faq.length; i++) {
         var body = this.nextElementSibling;
         if (body.style.display === "block") {
             body.style.display = "none";
-        } else {
-            body.style.display = "block";
         }
     });
-    faq[i].addEventListener("onclick", function () {
+}
+/*********faq starts here**************/
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
-        this.classList.toggle("hover");
+        this.classList.toggle("active");
         /* Toggle between hiding and showing the active panel */
         var body = this.nextElementSibling;
         if (body.style.display === "block") {
