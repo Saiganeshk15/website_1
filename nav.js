a45a8c5607245e25dc17b96bc039a63385fed17e
Nihanth <nihanthsripthi@gmail.com>
2023-01-28 10:48:34 +0530

    changes2

--- nav.js
+++ nav.js
@@ -1,56 +1,66 @@
-var x=window.matchMedia("(max-width: 768px)")
-var y=window.matchMedia("(min-width: 768px)")
-function ourfunction(y){
+var x = window.matchMedia("(max-width: 768px)")
+var y = window.matchMedia("(min-width: 768px)")
+function ourfunction(y) {
     var faq = document.getElementsByClassName("faq-page");
-var i;
-for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("mouseover", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("hover");
-        /* Toggle between hiding and showing the active panel */
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
-        }
-    });
-    faq[i].addEventListener("mouseout", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("hover");
-        /* Toggle between hiding and showing the active panel */
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        }
-    });
-}
+    var i;
+    for (i = 0; i < faq.length; i++) {
+        faq[i].addEventListener("mouseover", function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("hover");
+            /* Toggle between hiding and showing the active panel */
+            var body = this.nextElementSibling;
+            if (body.style.display === "block") {
+                body.style.display = "none";
+            } else {
+                body.style.display = "block";
+            }
+        });
+        faq[i].addEventListener("mouseout", function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("hover");
+            /* Toggle between hiding and showing the active panel */
+            var body = this.nextElementSibling;
+            if (body.style.display === "block") {
+                body.style.display = "none";
+            }
+        });
+    }
 }
 ourfunction(y)
 y.addEventListener(ourfunction)
 
 /*********faq starts here**************/
-function Myfunction(x){
+function Myfunction(x) {
     var faq = document.getElementsByClassName("faq-page");
     var i;
     for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-        /* Toggle between hiding and showing the active panel */
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else{
-            body.style.display = "block";
-        }
-    });
-}
+        faq[i].addEventListener("click", function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+            /* Toggle between hiding and showing the active panel */
+            var body = this.nextElementSibling;
+            if (body.style.display === "block") {
+                body.style.display = "none";
+            } else {
+                body.style.display = "block";
+            }
+        });
+    }
 }
 Myfunction(x)
 x.addEventListener(Myfunction)
 
 /**********faqs ends here*************/
+
+
+
+/************* pre loader *************/
+var loader = document.getElementById("loaders");
+
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+})
+
